5491312443542de392d4292acb83b9b3e35dc2c8
Serdar Tumgoren <zstumgoren@gmail.com>
2015-02-26 09:37:02 -0800

    clobber scripts dir because of shell var headaches; slice up bootstrap into 3 separate scripts b/c of sudo context switching

--- scripts/activate_process_control.sh
+++
@@ -1,4 +0,0 @@
-# Activate process control, which manages gunicorn and worker processes
-cp $CONFIG_PATH/files/supervisor.conf /etc/supervisor/conf.d/geomancer.conf
-supervisorctl reread
-supervisorctl update
--- scripts/update_bashrc.sh
+++
@@ -1,5 +0,0 @@
-# NOTE: One or more vars are inherited from bootstrap.sh!!
-
-cp $HOME/.bashrc $HOME/.bashrc_backup
-cat $CONFIG_PATH/files/bashrc_extras.sh >> $HOME/.bashrc
-source $HOME/.bashrc
